b274c15c9eabdaecd5da6e451020dcd929079360
rutko <t.222cm@gmail.com>
2023-06-25 01:18:02 +0900

    :+1:カテゴリーフォーム追加

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -46,32 +46,18 @@ export default function Index() {
   console.log(data)
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <h1>Synca1 Admin</h1>
+      <form method="post">
+        <fieldset>
+          <legend>カテゴリーの作成</legend>
+          <div>
+            <label htmlFor="name">カテゴリー名</label>
+            <input name="name" type="text" required />
+          </div>
+
+          <button type="submit">作成</button>
+        </fieldset>
+      </form>
     </div>
   );
 }
