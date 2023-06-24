7ad7253a875a63a6a7b0c86ca868e3e5381cda38
rutko <t.222cm@gmail.com>
2023-06-25 02:00:24 +0900

    検証

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -16,11 +16,12 @@ type NewCategries = InferModel<typeof categories, 'insert'>;
 export async function action({request, context}: ActionArgs) {
   const formData = await request.formData();
   const name = formData.get('name') as string;
+  const currentDate = new Date()
   const newCategries: NewCategries = {
     id: 1,
     name: name,
-    createdAt: new Date(),
-    updatedAt: new Date(),
+    createdAt: currentDate,
+    updatedAt: currentDate,
   }
   const db = createClient(context.DB as D1Database);
   await db.insert(categories).values(newCategries);
