7989e514ec83e8308c7abb156e56117077ce2679
rutko <t.222cm@gmail.com>
2023-06-27 00:18:19 +0900

    検証

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,12 @@
 import type { V2_MetaFunction } from "@remix-run/cloudflare";
-import { redirect } from '@remix-run/cloudflare';
+import { redirect, unstable_createMemoryUploadHandler, unstable_parseMultipartFormData } from '@remix-run/cloudflare';
 import type { LoaderArgs, ActionArgs } from '@remix-run/cloudflare';
 import { useLoaderData } from "@remix-run/react";
 import type { InferModel } from 'drizzle-orm';
 import { createClient } from "~/db/client.server"
-import { categories, tags } from '~/db/schema';
+import { categories, tags, images, imagesToTags } from '~/db/schema';
+import invariant from 'tiny-invariant';
+import { uuid } from '@cfworker/uuid';
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -13,19 +15,56 @@ export const meta: V2_MetaFunction = () => {
   ];
 };
 
-// type NewCategries = InferModel<typeof categories, 'insert'>;
-// export async function action({request, context}: ActionArgs) {
-//   const formData = await request.formData();
-//   const name = formData.get('name') as string;
-//   const newCategries: NewCategries = {
-//     name: name,
-//     createdAt: new Date(),
-//     updatedAt: new Date(),
-//   }
-//   const db = createClient(context.DB as D1Database);
-//   await db.insert(categories).values(newCategries).run();
-//   return redirect(`/`);
-// }
+type NewImage = InferModel<typeof categories, 'insert'>;
+type NewimagesToTags = InferModel<typeof imagesToTags, 'insert'>;
+export async function action({request, context}: ActionArgs) {
+
+  const uploadHandler = unstable_createMemoryUploadHandler({
+    maxPartSize: 1024 * 1024 * 10,
+  });
+
+  const form = await unstable_parseMultipartFormData(request, uploadHandler);
+
+  const file = form.get('file') as Blob;
+  invariant(file, 'File is required');
+
+  const fileName = `${uuid()}.${file.type.split('/')[1]}`;
+
+  const bucket = (context.MY_BUCKET as R2Bucket);
+  // R2バケットにアップロードする
+  const response = await bucket.put(fileName, await file.arrayBuffer(), {
+    httpMetadata: {
+      contentType: file.type,
+    },
+  });
+
+  const formData = await request.formData();
+  const name = formData.get('name') as string;
+  const category = formData.get('category');
+  const newImage: NewImage = {
+    key: response.key,
+    name: name,
+    categoryId: category,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  }
+
+  const db = createClient(context.DB as D1Database);
+  const imageResponse = await db.insert(images).values(newImage).run();
+  const tags = formData.get('tags');
+  const imageId = imageResponse.id
+
+
+  const newimagesToTags: NewimagesToTags = {
+    imageId: imageId,
+    tagId: tags,
+  }
+
+  await db.insert(imagesToTags).values(newimagesToTags).run();  
+
+
+  return redirect(`/`);
+}
 
 export const loader = async ({ context }: LoaderArgs) => {
   const db = createClient(context.DB as D1Database);
@@ -66,7 +105,7 @@ export default function Index() {
           </div>
           <div>
             <label htmlFor="categoryId">カテゴリー選択（必須）</label>
-            <select name="categoryId">
+            <select name="category">
               {data.categories.map((c)=> (
                 <option key={c.id} value={c.id}>{c.name}</option>
               ))}
