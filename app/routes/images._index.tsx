c82ee590749285410e61fd6c8a23446e12404a1e
rutko <t.222cm@gmail.com>
2023-06-29 00:27:33 +0900

    検証

--- app/routes/images._index.tsx
+++ app/routes/images._index.tsx
@@ -19,34 +19,34 @@ type newImages = InferModel<typeof images, 'insert'>;
 type newImagesToTags = InferModel<typeof imagesToTags, 'insert'>;
 export async function action({request, context}: ActionArgs) {
 
-  const uploadHandler = unstable_createMemoryUploadHandler({
-    maxPartSize: 1024 * 1024 * 10,
-  });
+  // const uploadHandler = unstable_createMemoryUploadHandler({
+  //   maxPartSize: 1024 * 1024 * 10,
+  // });
 
-  const form = await unstable_parseMultipartFormData(request, uploadHandler);
+  // const form = await unstable_parseMultipartFormData(request, uploadHandler);
 
-  const file = form.get('file') as Blob;
-  invariant(file, 'File is required');
+  // const file = form.get('file') as Blob;
+  // invariant(file, 'File is required');
 
-  const fileName = `${uuid()}.${file.type.split('/')[1]}`;
+  // const fileName = `${uuid()}.${file.type.split('/')[1]}`;
 
-  const bucket = (context.MY_BUCKET as R2Bucket);
-  // R2バケットにアップロードする
-  const response = await bucket.put(fileName, await file.arrayBuffer(), {
-    httpMetadata: {
-      contentType: file.type,
-    },
-  });
+  // const bucket = (context.MY_BUCKET as R2Bucket);
+  // // R2バケットにアップロードする
+  // const response = await bucket.put(fileName, await file.arrayBuffer(), {
+  //   httpMetadata: {
+  //     contentType: file.type,
+  //   },
+  // });
 
   const formData = await request.formData();
   const name = formData.get('name') as string;
   const categoryId = formData.get('categoryId');
   const newImage: newImages = {
-    key: response.key,
+    key: 'response.key',
     createdAt: new Date(),
     updatedAt: new Date(),
     name: name,
-    categoryId: categoryId,
+    categoryId: 1,
   }
 
   const db = createClient(context.DB as D1Database);
@@ -57,7 +57,7 @@ export async function action({request, context}: ActionArgs) {
 
   const newimagesToTags: newImagesToTags = {
     imageId: imageId,
-    tagId: tags,
+    tagId: 1,
   }
 
   await db.insert(imagesToTags).values(newimagesToTags).run();  
