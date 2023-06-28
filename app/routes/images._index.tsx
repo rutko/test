5a0c8f30548705bcb59060c419d0251308018078
rutko <t.222cm@gmail.com>
2023-06-29 00:19:54 +0900

    検証

--- app/routes/images._index.tsx
+++ app/routes/images._index.tsx
@@ -94,7 +94,7 @@ export default function Index() {
           <a href="/tags">タグ</a>
         </li>
       </ul>
-      <form method="post" action="/images" encType="multipart/form-data">
+      <form method="post" encType="multipart/form-data">
         <fieldset>
           <legend>画像のアップロード</legend>
           <div>
