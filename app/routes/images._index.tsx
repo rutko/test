64aa67b1347c648c8a7496d451332fdf6a9c65da
rutko <rutko@rutkos-MacBook-Air.local>
2023-07-10 00:23:49 +0900

    検証

--- app/routes/images._index.tsx
+++ app/routes/images._index.tsx
@@ -53,7 +53,7 @@ export async function action({request, context}: ActionArgs) {
       name: name,
       createdAt: new Date(),
       updatedAt: new Date(),
-      category_id: 1,
+      category_id: categoryId,
     }
     const db = createClient(context.DB as D1Database);
     const d1Response = await db.insert(images).values(newImage).run();
---
+++ migrations/seed.sql
@@ -0,0 +1,1 @@
+INSERT INTO images (id, key, name, category_id, createdAt, updatedAt) VALUES (2, "test key", "TEST", 9, "1688308063", "1688308063");
