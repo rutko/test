1fb9034a0be1108d9b3f99e4128c5b2b0e5043c4
rutko <rutko@rutkos-MacBook-Air.local>
2023-07-11 00:20:57 +0900

    検証

--- app/routes/images._index.tsx
+++ app/routes/images._index.tsx
@@ -53,7 +53,7 @@ export async function action({request, context}: ActionArgs) {
         name: name,
         createdAt: new Date(),
         updatedAt: new Date(),
-        category_id: categoryId,
+        category_id: 1,
       }
 
       const db = createClient(context.DB as D1Database);
