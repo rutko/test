7870e7c15146e5c3dc21d24cc1bd1547b83be6cb
rutko <rutko@rutkos-MacBook-Air.local>
2023-07-12 22:44:24 +0900

    検証

--- app/routes/images._index.tsx
+++ app/routes/images._index.tsx
@@ -55,8 +55,8 @@ export async function action({request, context}: ActionArgs) {
       }
 
       const db = createClient(context.DB as D1Database);
-      await db.insert(images).values(newImage).run()
-      return response
+      const d1Response = await db.insert(images).values(newImage).returning()
+      return d1Response
     });
 
     // Wait for all uploads to finish.
